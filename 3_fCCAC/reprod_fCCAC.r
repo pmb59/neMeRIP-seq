93504306769f86a180b7f884f7bee552b7dd14f0
Pedro Madrigal <dnaseiseq@gmail.com>
2018-01-11 00:20:36 +0000

    Update and rename fCCAC.R to reprod_fCCAC.r

---
+++ 3_fCCAC/reprod_fCCAC.r
@@ -0,0 +1,30 @@
+#Reproducibility assessment with Bioconductor package fCCAC 
+#
+# Madrigal P (2016). “fCCAC: functional canonical correlation analysis to 
+# evaluate covariance between nucleic acid sequencing datasets.” Bioinformatics, 2017
+#
+######################################################################
+#Use 24,877 peaks #
+
+
+library(fCCAC)
+
+######################################################################
+# m6A Ip
+######################################################################
+
+peaks <- "subset_fccac.bed"    
+bigwigs <- c("m6a_IP_A1.normalised.bw", "m6a_IP_A2.normalised.bw", "m6a_IP_A3.normalised.bw" , "m6a_IP_S1.normalised.bw",  "m6a_IP_S2.normalised.bw",  "m6a_IP_S3.normalised.bw" )  
+labels <- c( "m6A Activin", "m6A Activin","m6A Activin"  , "m6A SB", "m6A SB","m6A SB"  )  
+x <- fCCAC(main="m6A MeRIP-seq peaks", peaks=peaks, bigwigs=bigwigs, labels=labels ,  splines=15, nbins=100, ncan=2)   #ncan < splines
+#head(x)
+
+#heatmap
+pdf("reproducibility_F_values_heatmap_IP_24877peaks.pdf", height=5, width=5.5)
+heatmapfCCAC(x)
+dev.off()
+
+
+
+
+
