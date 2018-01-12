355608aa49b808d1c0dd53fdb3c6c4381ff47df9
Pedro Madrigal <dnaseiseq@gmail.com>
2018-01-12 02:12:11 +0000

    Update MACS.sh

--- 5_MACS/MACS.sh
+++ 5_MACS/MACS.sh
@@ -6,9 +6,9 @@ ip2="../m6A/tophat_m6a_IP_A2/m6a_IP_A2.bam"
 ip3="../m6A/tophat_m6a_IP_A3/m6a_IP_A3.bam"
 input="../m6A/tophat_m6a_input2_A/m6a_input2_A.bam"
 
-macs2 callpeak -t $ip1 -c $input -f BAM --outdir $OUT -q 0.001 --nomodel --keep-dup all -n A1
-macs2 callpeak -t $ip2 -c $input -f BAM --outdir $OUT -q 0.001 --nomodel --keep-dup all -n A2
-macs2 callpeak -t $ip3 -c $input -f BAM --outdir $OUT -q 0.001 --nomodel --keep-dup all -n A3
+macs2 callpeak -t $ip1 -c $input -f BAM --outdir $OUT -q 0.05 --nomodel --keep-dup all -n A1
+macs2 callpeak -t $ip2 -c $input -f BAM --outdir $OUT -q 0.05 --nomodel --keep-dup all -n A2
+macs2 callpeak -t $ip3 -c $input -f BAM --outdir $OUT -q 0.05 --nomodel --keep-dup all -n A3
 
 #SB
 ip1="../m6A/tophat_m6a_IP_S1/m6a_IP_S1.bam"
@@ -16,9 +16,9 @@ ip2="../m6A/tophat_m6a_IP_S2/m6a_IP_S2.bam"
 ip3="../m6A/tophat_m6a_IP_S3/m6a_IP_S3.bam"
 input="../m6A/tophat_m6a_input2_S/m6a_input2_S.bam"
 
-macs2 callpeak -t $ip1 -c $input -f BAM --outdir $OUT -q 0.001 --nomodel --keep-dup all -n S1
-macs2 callpeak -t $ip2 -c $input -f BAM --outdir $OUT -q 0.001 --nomodel --keep-dup all -n S2
-macs2 callpeak -t $ip3 -c $input -f BAM --outdir $OUT -q 0.001 --nomodel --keep-dup all -n S3
+macs2 callpeak -t $ip1 -c $input -f BAM --outdir $OUT -q 0.05 --nomodel --keep-dup all -n S1
+macs2 callpeak -t $ip2 -c $input -f BAM --outdir $OUT -q 0.05 --nomodel --keep-dup all -n S2
+macs2 callpeak -t $ip3 -c $input -f BAM --outdir $OUT -q 0.05 --nomodel --keep-dup all -n S3
 
 
 
